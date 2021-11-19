a0a1a9233238708f4a2f061a1258e2a0303b117a
saltsphinx <tremar.thomas19@gmail.com>
2021-11-19 11:24:18 -0600

    Create test for #move_piece in spec/board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,14 +1,20 @@
+require_relative '../lib/board'
+
 describe Board do
-  describe '#take_piece' do
-    #board.take_piece(piece_pos, destination_pos)
-    #Command method, test that piece take destination piece's square
+  describe '#move_piece' do
+    #board.move_piece(piece_pos, destination_pos)
+    #Command method, test that piece takes destination piece's square and is replaced with nil
     context 'when there\'s a piece on both squares' do
-      board = {:a1: 'knight', :a2: 'rook'}
-      subject(:board_take) { described_class.new(board) }
+      board = {a1: 'knight', a2: 'rook'}
+      subject(:board_move) { described_class.new(board) }
 
       it 'moves piece to destination and replaces it with nil' do
-        board_take.setup
-
+        expected_board = {
+          a1: nil,
+          a2: 'knight'
+        }
+        board_move.move_piece(:a1, :a2)
+        expect(board_move.board).to eq(expected_board)
       end
     end
   end
