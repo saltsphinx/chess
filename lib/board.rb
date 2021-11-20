0b2e81f39c6aeb90f0c9916088f3c3f6cf76bd6c
saltsphinx <tremar.thomas19@gmail.com>
2021-11-19 20:47:21 -0600

    Create #display_board and remove some testing code in lib/board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,9 @@
 class Board
   attr_reader :board
 
+  UNI_PREFIX_WHITE = {ki: "\u265a", qu: "\u265b", ro: "\u265c", bi: "\u265d", kn: "\u265e", pa: "\u2659"}
+  UNI_PREFIX_BLACK = {ki: "\u2654", qu: "\u2655", ro: "\u2656", bi: "\u2657", kn: "\u2658", pa: "\u265f"}
+
   def initialize(board = {})
     @board = board
   end
@@ -24,11 +27,10 @@ class Board
     destination = @board[destination_pos]
 
     return if piece_pos.nil?
-    return move_piece(piece_pos, destination_pos) if destination == nil || piece[-1] != destination[-1]
+    move_piece(piece_pos, destination_pos) if destination == nil || piece[-1] != destination[-1]
   end
 
   def move_piece(piece_pos, destination_pos)
-    p @board
     @board[destination_pos] = @board[piece_pos]
     @board[piece_pos] = nil
   end
@@ -61,6 +63,20 @@ class Board
         symbol = "#{file << (rank + 1).to_s}".to_sym
         @board[symbol] = nil
       end
+    end
+  end
+
+  def display_board
+    (1..8).reverse_each do |rank|
+      puts "  a b c d e f g h\n" if rank == 8
+      print "#{rank}"
+      ('a'..'h').each do |file|
+        square = @board[(file + rank.to_s).to_sym]
+        next print('  ') if square == nil
+        square[-1] == 'w' ? print((' ' + UNI_PREFIX_WHITE[square[0..1].to_sym])) : print((' ' + UNI_PREFIX_BLACK[square[0..1].to_sym]))
+      end
+      print " #{rank}\n"
+      puts "  a b c d e f g h" if rank == 1
     end
   end
 end
