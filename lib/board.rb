f7125f4b7780903e4cdbfee85c040cacb67d9dff
saltsphinx <tremar.thomas19@gmail.com>
2021-11-19 21:11:22 -0600

    Use rubocop on lib/board

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
+# Chess board class, the playing ground, piece generator/mover, and board visualize
 class Board
   attr_reader :board
 
-  UNI_PREFIX_WHITE = {ki: "\u265a", qu: "\u265b", ro: "\u265c", bi: "\u265d", kn: "\u265e", pa: "\u2659"}
-  UNI_PREFIX_BLACK = {ki: "\u2654", qu: "\u2655", ro: "\u2656", bi: "\u2657", kn: "\u2658", pa: "\u265f"}
+  UNI_PREFIX_WHITE = { ki: "\u265a", qu: "\u265b", ro: "\u265c", bi: "\u265d", kn: "\u265e", pa: "\u2659" }
+  UNI_PREFIX_BLACK = { ki: "\u2654", qu: "\u2655", ro: "\u2656", bi: "\u2657", kn: "\u2658", pa: "\u265f" }
 
   def initialize(board = {})
     @board = board
@@ -27,7 +30,8 @@ class Board
     destination = @board[destination_pos]
 
     return if piece_pos.nil?
-    move_piece(piece_pos, destination_pos) if destination == nil || piece[-1] != destination[-1]
+
+    move_piece(piece_pos, destination_pos) if destination.nil? || piece[-1] != destination[-1]
   end
 
   def move_piece(piece_pos, destination_pos)
@@ -36,12 +40,12 @@ class Board
   end
 
   def place_main
-    main = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
+    main = %w[rook knight bishop queen king bishop knight rook]
 
     [1, 8].each do |rank|
       ('a'..'h').each_with_index do |file, index|
-        symbol = "#{file << rank.to_s}".to_sym
-        rank == 1 ? @board[symbol] = (main[index] + 'w').to_sym : @board[symbol] = (main[index] + 'b').to_sym
+        symbol = (file << rank.to_s).to_sym
+        @board[symbol] = rank == 1 ? (main[index] + 'w').to_sym : (main[index] + 'b').to_sym
       end
     end
   end
@@ -51,8 +55,8 @@ class Board
 
     [2, 7].each do |rank|
       ('a'..'h').each_with_index do |file, index|
-        symbol = "#{file << rank.to_s}".to_sym
-        rank == 2 ? @board[symbol] = (pawns[index] + 'w').to_sym : @board[symbol] = (pawns[index] + 'b').to_sym
+        symbol = (file << rank.to_s).to_s.to_sym
+        @board[symbol] = rank == 2 ? (pawns[index] + 'w').to_sym : (pawns[index] + 'b').to_sym
       end
     end
   end
@@ -60,7 +64,7 @@ class Board
   def generate_board_squares
     8.times.each do |rank|
       ('a'..'h').each do |file|
-        symbol = "#{file << (rank + 1).to_s}".to_sym
+        symbol = (file << (rank + 1).to_s).to_s.to_sym
         @board[symbol] = nil
       end
     end
@@ -69,14 +73,15 @@ class Board
   def display_board
     (1..8).reverse_each do |rank|
       puts "  a b c d e f g h\n" if rank == 8
-      print "#{rank}"
+      print rank.to_s
       ('a'..'h').each do |file|
         square = @board[(file + rank.to_s).to_sym]
-        next print('  ') if square == nil
+        next print('  ') if square.nil?
+
         square[-1] == 'w' ? print((' ' + UNI_PREFIX_WHITE[square[0..1].to_sym])) : print((' ' + UNI_PREFIX_BLACK[square[0..1].to_sym]))
       end
       print " #{rank}\n"
-      puts "  a b c d e f g h" if rank == 1
+      puts '  a b c d e f g h' if rank == 1
     end
   end
 end
